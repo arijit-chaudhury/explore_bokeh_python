548b756bd405bef8b121738a1f4951c878c397b1
ARIJIT CHAUDHURY <34763856+arijit-chaudhury@users.noreply.github.com>
2025-01-18 20:32:08 +0000

    minor change of input files

--- modeling/bokeh/__init__.py
+++ modeling/bokeh/__init__.py
@@ -2,10 +2,10 @@ from bokeh.plotting import figure
 from bokeh.io import show, output_file
 import pandas as pd
 
-df = pd.read_csv("../../organizations-1000.csv")
+df = pd.read_csv("../../mpg.csv")
 
 output_file("__init__.html")
 fig = figure()
-fig.line(df['Founded'], df['Number of employees'])
+fig.line(df['horsepower'], df['model_year'])
 
 show(fig)
